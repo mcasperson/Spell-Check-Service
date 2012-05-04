52e68eac6891d7aaf92aa51bb7934863a65463b0
Matthew Casperson <mcaspers@redhat.com>
2012-05-04 14:35:53 +1000

    Added maven package plugin

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# For some reason using the Maven shade or assembly plug-ins results in 
+# an infinite loop, and a stack overflow error. The maven-dependency-plugin
+# is used to collate the JAR files required for this application to run.
+# In addition, the Hunspell library requires access to the dictionaries as 
+# files, which means we can't have a self contained JAR file anyway.
+
+# To deploy this application run mvn compile and mvn package, 
+# copy the target/classes and target/lib files to the destination directory, 
+# and run this script file. 
+
 #STOMP_SERVER=localhost
 #REST_SERVER=http://localhost:8080/TopicIndex/seam/resource/rest
 
@@ -8,9 +18,10 @@ REST_SERVER=http://skynet.usersys.redhat.com:8080/TopicIndex/seam/resource/rest
 PORT=61613
 USER=guest
 PASS=guest
-QUEUE=jms.queue.SkynetDocbookBuildQueue
+QUEUE=none
 MAINCLASS=com.redhat.topicindex.syntaxchecker.Main
-QUERY=query;
+#QUERY="query;topicEditedInLastDays=2"
+QUERY="query;tag19=1;tag119=1;catint5=And;tag133=1;tag132=1;tag14=0"
 
 java \
 -DtopicIndex.stompMessageServer=${STOMP_SERVER} \
@@ -20,5 +31,4 @@ java \
 -DtopicIndex.stompMessageServerQueue=${QUEUE} \
 -DtopicIndex.skynetServer=${REST_SERVER} \
 -DtopicIndex.spellCheckQuery=${QUERY} \
--jar spellchecker-service.jar
-
+-cp ./target/classes:./target/lib/* ${MAINCLASS}
